35d10911f9d0e0c9c3592d0b4d6582bff9b156c4
Vagner <105022247+v4gn3r@users.noreply.github.com>
2025-09-25 21:10:30 -0300

    implementando a lógica de programação

--- primeiro html/main.js
+++ primeiro html/main.js
@@ -1,3 +1,15 @@
-let nome_usuario  = prompt("Qual o seu nome?");
+let nome_usuario  = "";
 let elemento = document.querySelector("#nome-usuário");
-elemento.textContent = nome_usuario;
+
+while(nome_usuario == ""){
+    nome_usuario  = prompt("Qual o seu nome?");
+}
+
+if( nome_usuario==null){
+    elemento.textContent = "Seja muito bem vindo!!";
+}
+
+else{
+    elemento.textContent = nome_usuario;
+}
+
